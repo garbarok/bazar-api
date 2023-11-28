73015738305b8c0a9cafc53d31b700d0c64e889f
Oscar <info@oscargallegoruiz.com>
2023-11-28 13:25:21 +0100

    Revert "Test Middleware"
    This reverts commit 2e69035036f72bd68b8860d4a8e4f2a9ce488a4c.

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes/index');
-const {ipFilter} = require('./middleware/middelware');
 require("dotenv").config();
 
 const app = express();
@@ -8,7 +7,6 @@ const PORT = process.env.PORT ?? 3000;
 
 app.disable('x-powered-by');
 app.use(express.json());
-app.use(ipFilter);
 app.use('/api', routes);
 app.use((req, res) => {
   res.status(404).send('Not found');
